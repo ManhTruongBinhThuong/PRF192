48b3d29d501e94b7b1b58626a216aedf4e924b3b
Your Name <you@example.com>
2026-01-09 11:32:01 +0700

    so thuc a + b

--- slot 2/so thuc a + b.c
+++ slot 2/so thuc a + b.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main(){
 	//khai bao 1 bien so thuc a, b, khoi tao gia tri
-	float a, b, sum;
+	float a, b ,sum;
 	a = 18;
+	printf("a = %.1f\n", a);
 	b = 0.36;
+	printf("b = %.2f\n", b);
 	sum = a + b;
-	printf("Ket qua : %.2f", sum);
+	printf("%.1f + %.2f = %.2f" , a, b, sum);
 	return 0;
 }
