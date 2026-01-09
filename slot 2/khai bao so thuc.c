d2012a5e90aecc55cc4c55131baa2f09cea04b29
Your Name <you@example.com>
2026-01-09 10:59:15 +0700

    khai bao so nguyen

---
+++ slot 2/khai bao so thuc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	//khai bao 1 bien so thuc
+	float a;
+	//khoi tao gia tri bat ky
+	a = 18.36;
+	//in ra man hinh
+	printf("gia tri cua a la : %.2f", a);
+	return 0;
+}
