fe2af89fac16e92f19c101600797e92de1b139c6
Your Name <you@example.com>
2026-01-09 11:22:36 +0700

    so thuc a + b

---
+++ slot 2/so thuc a + b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	//khai bao 1 bien so thuc a, b, khoi tao gia tri
+	float a, b, sum;
+	a = 18;
+	b = 0.36;
+	sum = a + b;
+	printf("Ket qua : %.2f", sum);
+	return 0;
+}
