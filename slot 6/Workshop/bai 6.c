176671942bb8e3ec6f3b437ee8dbe51c4111cac3
ManhTruongBinhThuong <payaccghoul@gmail.com>
2026-01-23 11:46:52 +0700

    Update bai 6.c

--- slot 6/Workshop/bai 6.c
+++ slot 6/Workshop/bai 6.c
@@ -7,13 +7,13 @@ int main() {
     printf("Nhap diem chuyen can, giua ky, cuoi ky:");
 	scanf ("%f %f %f", &diemChuyencan, &diemGiuaky, &diemCuoiky);
 	diemTongKet =((diemChuyencan * 0.1) + (diemGiuaky * 0.3) + (diemCuoiky * 0.5));
-    if (diemTongKet >= 8.5)
+    if (diemTongKet >= 8.5 && diemTongKet <= 10)
         diemChu = 'A';
-    else if (diemTongKet >= 7.0)
+    else if (diemTongKet >= 7.0 && diemTongKet < 8.5)
         diemChu = 'B';
-    else if (diemTongKet >= 5.5)
+    else if (diemTongKet >= 5.5 && diemTongKet < 7.0)
         diemChu = 'C';
-    else if (diemTongKet >= 4.0)
+    else if (diemTongKet >= 4.0 && diemTongKet < 5.5)
         diemChu = 'D';
     else
         diemChu = 'F';
