12cbd7ed3552bd193cc663cfc919b6a7d129a1a7
Your Name <you@example.com>
2026-01-23 11:36:40 +0700

    slot 6

---
+++ slot 6/Workshop/bai 5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+	float soKm;
+	int money;
+	int thoiGian;
+	float tong;
+	printf("nhap quang duong (km): ");
+	scanf("%f", &soKm);
+	if (soKm <= 0){
+		printf("nhap sai, dung chuong trinh\n");
+		return 0;
+ 	}
+	printf("thoi gian cho(phut): ");
+	scanf("%d", &thoiGian);
+	if (soKm <= 0.5){
+		money = 12000;
+	} else if (soKm <= 30 && soKm >= 0.5){
+		money = 12000 + (soKm - 0.5) * 15000;
+	} else {
+		money = 12000 + ((30 - 0.5) * 15000) + ((soKm - 30)* 12000);
+	} 
+	tong = money;
+	if (thoiGian > 5){
+		tong = money + (thoiGian - 5) * 1000;
+	}
+	printf("Tong tien taxi: %.0f \n", tong);
+	return 0;
+}
---
+++ slot 6/Workshop/bai 6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    float diemChuyencan, diemGiuaky, diemCuoiky;
+    float diemTongKet;
+    char diemChu;
+    printf("Nhap diem chuyen can, giua ky, cuoi ky:");
+	scanf ("%f %f %f", &diemChuyencan, &diemGiuaky, &diemCuoiky);
+	diemTongKet =((diemChuyencan * 0.1) + (diemGiuaky * 0.3) + (diemCuoiky * 0.5));
+    if (diemTongKet >= 8.5)
+        diemChu = 'A';
+    else if (diemTongKet >= 7.0)
+        diemChu = 'B';
+    else if (diemTongKet >= 5.5)
+        diemChu = 'C';
+    else if (diemTongKet >= 4.0)
+        diemChu = 'D';
+    else
+        diemChu = 'F';
+    printf("\nDiem tong ket: %.2f", diemTongKet);
+    printf("\nDiem chu: %c", diemChu);
+    if (diemChuyencan >= 4.0 && diemGiuaky >= 4.0 && diemCuoiky >= 4.0 && diemChu != 'F') {
+        printf("\n=> Du dieu kien tot nghiep");
+    } else {
+        printf("\n=> Khong du dieu kien tot nghiep");
+    }
+
+    return 0;
+}
