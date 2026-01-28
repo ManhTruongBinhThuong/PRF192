38733ad0451ef85ab43778f0c40b7409e9973476
ManhTruongBinhThuong <payaccghoul@gmail.com>
2026-01-28 07:22:37 +0700

    Update ex4.c

--- slot 5/ex4.c
+++ slot 5/ex4.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-int main(){
+
+int main() {
 	int money;
 	int soKwh;
 	printf("Nhap so Kwh: ");
 	scanf("%d", &soKwh);
-
-	if ( soKwh <= 100 ){
+	if(soKwh <= 100) {
 		money = soKwh * 950;
-
-	} else if(soKwh <= 150 && soKwh > 100){
-		money = soKwh * 1250;
-	} else if(soKwh <= 200 && soKwh > 150){
-		money = soKwh * 1350;
-	} else 
-		money = soKwh * 1550;
-	printf("so tien la : %d", money);
+	} else if(soKwh <= 150) {
+		money = 100 * 950 + (soKwh - 100) * 1250;
+	} else if(soKwh <= 200) {
+		money = 100 * 950 + 50 * 1250 + (soKwh - 150) * 1350;
+	} else{
+		money = 100 * 950 + 50 * 1250 + 50 * 1350 + (soKwh - 200) * 1550;
+	}
+	printf("So tien phai tra la: %d", money);
 	return 0;
-		
 }
