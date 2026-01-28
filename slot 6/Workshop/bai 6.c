1f26548f0f5239550ac8fc730f4e139c3bad14ad
ManhTruongBinhThuong <payaccghoul@gmail.com>
2026-01-28 09:10:43 +0700

    Update bai 6.c

--- slot 6/Workshop/bai 6.c
+++ slot 6/Workshop/bai 6.c
@@ -5,15 +5,21 @@ int main() {
     float diemTongKet;
     char diemChu;
     printf("Nhap diem chuyen can, giua ky, cuoi ky:");
-	scanf ("%f %f %f", &diemChuyencan, &diemGiuaky, &diemCuoiky);
-	diemTongKet =((diemChuyencan * 0.1) + (diemGiuaky * 0.3) + (diemCuoiky * 0.5));
-    if (diemTongKet >= 8.5 && diemTongKet <= 10)
+	int kq = scanf("%f %f %f",&diemChuyencan, &diemGiuaky, &diemCuoiky);
+	printf("kq nhap: %d",kq);
+	if(kq !=3 || diemChuyencan <0 || diemGiuaky<0 || diemChuyencan <0 || diemCuoiky<0 || diemChuyencan >10 || diemGiuaky>10 ||diemCuoiky>10)
+	{
+		printf("\nNhap sai diem, diem >=0!");
+		return 0;
+	}
+	diemTongKet = (diemChuyencan * 0.1) + (diemGiuaky * 0.3) + (diemCuoiky * 0.6);
+    if (diemTongKet >= 8.5)
         diemChu = 'A';
-    else if (diemTongKet >= 7.0 && diemTongKet < 8.5)
+    else if (diemTongKet >= 7.0)
         diemChu = 'B';
-    else if (diemTongKet >= 5.5 && diemTongKet < 7.0)
+    else if (diemTongKet >= 5.5)
         diemChu = 'C';
-    else if (diemTongKet >= 4.0 && diemTongKet < 5.5)
+    else if (diemTongKet >= 4.0)
         diemChu = 'D';
     else
         diemChu = 'F';
